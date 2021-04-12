474980d07c32e3e9a3ff08ef7fe23f8bbcb03dd3
OM Miles <olivia.m.miles@gmail.com>
2021-04-12 07:01:34 -0700

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+/* Using the innerText (Links to an external site.) property to modify DOM element content will not work for this lab. Use textContent (Links to an external site.) or innerHTML (Links to an external site.) instead. */ 
+document.addEventListener(`DOMContentLoaded`, function() {
+    const p = document.getElementById(`text`)
+    p.innerText = `This is really cool!`
+})
